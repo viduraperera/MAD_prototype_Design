6f5445f5dc43e2c01ab9c8a1d0ca95304bdfeb7e
vidura umayanaga perera <laumayangaperera@gmail.com>
2020-08-16 01:03:05 +0530

    creating the a toast and an action button

--- app/src/main/java/com/example/prototype_design_mad/trending_recipes.java
+++ app/src/main/java/com/example/prototype_design_mad/trending_recipes.java
@@ -2,7 +2,12 @@ package com.example.prototype_design_mad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class trending_recipes extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class trending_recipes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trending_recipes);
+
+        FloatingActionButton floatingActionButton = findViewById(R.id.create);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast();
+            }
+        });
+    }
+    public void showToast(){
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, "Creating new recipe", duration); toast.show();
     }
 }
