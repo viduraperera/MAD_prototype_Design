8d02f1099e575d6aebf74bfc30fe86f2e3da1eba
ASUS <onellanatalie824@gmail.com>
2020-08-16 17:24:49 +0530

    Create_recipe_commit

---
+++ app/src/main/java/com/example/prototype_design_mad/create_recipe_myrecipe.java
@@ -0,0 +1,14 @@
+package com.example.prototype_design_mad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class create_recipe_myrecipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_recipe_myrecipe);
+    }
+}
