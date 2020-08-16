8f99e74634f2ba146a51fb9ea288db22a91f6038
vidura umayanaga perera <laumayangaperera@gmail.com>
2020-08-16 15:42:44 +0530

    decription_ingregients is finish

--- app/src/main/java/com/example/prototype_design_mad/saving_procedure.java
+++ app/src/main/java/com/example/prototype_design_mad/saving_procedure.java
@@ -2,7 +2,11 @@ package com.example.prototype_design_mad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class saving_procedure extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class saving_procedure extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saving_procedure);
+
+
+        Button savebutton = findViewById(R.id.save);
+        savebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast();
+            }
+        });
+    }
+    public void showToast(){
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, "Saving Recipe", duration); toast.show();
     }
 }
