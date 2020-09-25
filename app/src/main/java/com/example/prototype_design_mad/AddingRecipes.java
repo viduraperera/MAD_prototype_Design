e3e9f8bd5a0383ebbdd5997fc971bd86b9c91d0d
vidura umayanaga perera <laumayangaperera@gmail.com>
2020-09-26 01:19:15 +0530

    adding the single view to  recipes

--- app/src/main/java/com/example/prototype_design_mad/AddingRecipes.java
+++ app/src/main/java/com/example/prototype_design_mad/AddingRecipes.java
@@ -8,6 +8,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,14 +25,13 @@ import com.google.firebase.storage.UploadTask;
 
 public class AddingRecipes extends AppCompatActivity {
 
-    FirebaseDatabase mDatabase;
-    DatabaseReference mRef;
-    FirebaseStorage mStorage;
     ImageButton imageButton;
-    EditText title, ingredient, description, steps;
-    Button btn_insert;
-    private static final int Gallery_Code = 1;
+    EditText title, ingredients, description, steps;
+    Button submitBtn;
+    private  static final int GALLERY_REQUEST = 1;
     Uri imageUrl = null;
+    StorageReference mStorage;
+    DatabaseReference mDatabaseReference;
     ProgressDialog progressDialog;
 
     @Override
@@ -39,79 +39,81 @@ public class AddingRecipes extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adding_recipes);
 
-        title = findViewById(R.id.add_title);
-        ingredient = findViewById(R.id.add_ingredients);
-        description = findViewById(R.id.add_description);
-        steps = findViewById(R.id.add_steps);
-        imageButton = findViewById(R.id.add_image);
-        btn_insert = findViewById(R.id.add_submit);
+        mStorage = FirebaseStorage.getInstance().getReference();
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference().child("Recipes");
 
-        mDatabase = FirebaseDatabase.getInstance();
-        mRef = mDatabase.getReference().child("Recipes");
-        mStorage = FirebaseStorage.getInstance();
+        imageButton = (ImageButton) findViewById(R.id.add_image);
+        title = (EditText) findViewById(R.id.add_title);
+        ingredients = (EditText) findViewById(R.id.add_ingredients);
+        description = (EditText) findViewById(R.id.add_description);
+        steps = (EditText) findViewById(R.id.add_steps);
+        submitBtn = (Button) findViewById(R.id.add_submit);
+
         progressDialog = new ProgressDialog(this);
 
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("image/*");
-                startActivityForResult(intent, Gallery_Code);
+
+                Intent gallery_intent = new Intent(Intent.ACTION_GET_CONTENT);
+                gallery_intent.setType("image/*");
+                startActivityForResult(gallery_intent, GALLERY_REQUEST);
             }
         });
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addRecipe();
+            }
+        });
+
+    }
+
+    private void addRecipe(){
+
+        progressDialog.setMessage("adding Recipe.....");
+        progressDialog.show();
+
+        final String title_val = title.getText().toString();
+        final String description_val = description.getText().toString();
+        final String ingredient_val = ingredients.getText().toString();
+        final String steps_val = steps.getText().toString();
+
+        if(!TextUtils.isEmpty(title_val) && !TextUtils.isEmpty(description_val) && !TextUtils.isEmpty(ingredient_val) && !TextUtils.isEmpty(steps_val) && imageUrl != null){
+            StorageReference filePath = mStorage.child("Recipe").child(imageUrl.getLastPathSegment());
+
+            filePath.putFile(imageUrl).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    Task<Uri> downloadUrl = taskSnapshot.getStorage().getDownloadUrl();
+
+                    DatabaseReference newPost = mDatabaseReference.push();
+
+                    newPost.child("Title").setValue(title_val);
+                    newPost.child("Description").setValue(description_val);
+                    newPost.child("Ingredient").setValue(ingredient_val);
+                    newPost.child("Steps").setValue(steps_val);
+                    newPost.child("image").setValue(downloadUrl.toString());
+
+                    progressDialog.dismiss();
+
+                    Intent intent = new Intent(AddingRecipes.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == Gallery_Code && resultCode == RESULT_OK){
+        if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK){
             imageUrl = data.getData();
+
             imageButton.setImageURI(imageUrl);
-        }
-
-        btn_insert.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                    add_recipe();
-            }
-        });
-    }
-
-    private void add_recipe(){
-        final String on_title = title.getText().toString().trim();
-        final String on_ingredient = ingredient.getText().toString().trim();
-        final String on_description = description.getText().toString().trim();
-        final String on_steps = steps.getText().toString().trim();
-
-        if(!(on_title.isEmpty() && on_steps.isEmpty() && on_description.isEmpty() && on_ingredient.isEmpty() && imageUrl != null)){
-            progressDialog.setTitle("uploading........");
-            progressDialog.show();
-
-            StorageReference filepath = mStorage.getReference().child("imagePost").child(imageUrl.getLastPathSegment());
-            filepath.putFile(imageUrl).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    Task<Uri> downloadUrl = taskSnapshot.getStorage().getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Uri> task) {
-                            String t = task.getResult().toString();
-
-                            DatabaseReference newPost = mRef.push();
-
-                            newPost.child("Title").setValue(on_title);
-                            newPost.child("Ingredient").setValue(on_ingredient);
-                            newPost.child("Description").setValue(on_description);
-                            newPost.child("Steps").setValue(on_steps);
-                            newPost.child("Image").setValue(task.getResult().toString());
-                            progressDialog.dismiss();
-
-                            startActivity(new Intent(AddingRecipes.this, MainActivity.class));
-
-                        }
-                    });
-                }
-            });
         }
     }
 }
--- app/src/main/java/com/example/prototype_design_mad/MainActivity.java
+++ app/src/main/java/com/example/prototype_design_mad/MainActivity.java
@@ -8,18 +8,24 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerViewList;
     FirebaseRecyclerAdapter<RecipeModel, RecipeViewHolder> firebaseRecyclerAdapter;
     DatabaseReference mDatabase;
+    boolean mProcessLike = false;
+    DatabaseReference mDatabaseLike;
+    FirebaseAuth mAuth;
     FirebaseAuth.AuthStateListener mAuthListener;
 
 
@@ -36,6 +45,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mDatabase = FirebaseDatabase.getInstance().getReference().child("Recipes");
+        mDatabaseLike = FirebaseDatabase.getInstance().getReference().child("Likes");
+
+        mDatabaseLike.keepSynced(true);
 
         recyclerViewList = (RecyclerView) findViewById(R.id.list_recipe);
         recyclerViewList.setHasFixedSize(true);
@@ -55,9 +67,50 @@ public class MainActivity extends AppCompatActivity {
             @Override
             protected void populateViewHolder(RecipeViewHolder recipeViewHolder, RecipeModel recipeModel, int i) {
 
+                final String post_key = getRef(i).getKey();
+
                 recipeViewHolder.setTitle(recipeModel.getTitle());
                 recipeViewHolder.setDec(recipeModel.getDescription());
                 recipeViewHolder.setImage(getApplicationContext(), recipeModel.getImage());
+
+                recipeViewHolder.mView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                       // Toast.makeText(MainActivity.this, post_key, Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(MainActivity.this, SingleViewActivity.class);
+                        intent.putExtra("Recipe_id", post_key);
+                        startActivity(intent);
+                    }
+
+
+                });
+
+                recipeViewHolder.mLike.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                            mProcessLike = true;
+
+                            if(mProcessLike){
+                                mDatabaseLike.addValueEventListener(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                        if(dataSnapshot.child(post_key).hasChild(mAuth.getCurrentUser().getUid())){
+
+                                        }
+                                        else {
+                                            mDatabaseLike.child(post_key).child(mAuth.getCurrentUser().getUid()).setValue("RandomValue");
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                    }
+                                });
+                            }
+                    }
+                });
 
             }
         };
@@ -69,10 +122,16 @@ public class MainActivity extends AppCompatActivity {
 
         View mView;
 
+        ImageButton mLike;
+
         public RecipeViewHolder(@NonNull View itemView) {
             super(itemView);
 
             mView = itemView;
+
+            mLike = (ImageButton) mView.findViewById(R.id.recipe_like_btn);
+
+
         }
         public void setTitle(String title){
             TextView post_title = (TextView) mView.findViewById(R.id.single_title);
